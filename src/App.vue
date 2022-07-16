260d37facc9e5a0e1897b3f603d8117f7ab3f2e5
SirnaEls <nassir.elabbassi@yahoo.fr>
2022-07-16 17:38:27 +0200

    Creation of the project and the navbar

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import navv from './components/navv.vue'
+</script>
+
+<template>
+  <header>
+    <navv/>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/navv.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="navvbar">
+    <div>
+      <img src="/src/assets/LogoAlternCV.png" alt="" />
+    </div>
+    <div class="navigation">
+      <ul id="navbar">
+        <li> Le CV</li>
+        <li> À propos</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  background: black;
+}
+
+img {
+  width: 200px;
+  height: auto;
+  margin: 20px;
+}
+
+.navvbar{
+  display: flex;
+  justify-content: space-between;
+  margin-right: 140px;
+  margin-top: 20px;
+  box-sizing: border-box;
+}
+
+.navigation{
+ 
+  font-size: 20px;
+  color:white;
+
+}
+
+#navbar{
+  list-style: none;
+  display: flex;
+  gap: 40px;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+ 
+}
+</style>
